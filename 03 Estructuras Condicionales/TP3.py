497620d8848a10655551d8723d203d4ca87357ab
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2025-04-13 19:32:26 -0300

    Ejercicio 2

--- 03 Estructuras Condicionales/TP3.py
+++ 03 Estructuras Condicionales/TP3.py
@@ -9,3 +9,16 @@ if edad >= 18:
     print("Eres Mayor de edad")
 else:
     print("Eres menor de edad")
+
+#2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá 
+#mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el 
+#mensaje “Desaprobado”.
+
+print("Colocar tu nota")
+
+nota = float(input())
+
+if nota >= 6:
+    print("Estas aprobado")
+else:
+    print("Estas desaprobado")
