39e92d2262a74829ce169f714407be1c623df9de
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2025-04-17 20:05:04 -0300

    TP4 - Ejercicio 3

--- 04 Estructuras Repetitivas/TP4.py
+++ 04 Estructuras Repetitivas/TP4.py
@@ -4,3 +4,27 @@ i=0
 for i in range(0, 101):
     print(i)
 
+# 2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de 
+# dígitos que contiene.
+
+num = int(input("Indicar tu numero"))
+digi = str(num)
+cantidad= len(digi)
+print(" La cantidad de digitos de tu numero es: ", cantidad)
+
+# 3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores 
+# dados por el usuario, excluyendo esos dos valores.
+
+num1= int(input("Indicar el mayor de tus numeros"))
+num2= int(input("Indicar el menor de tus numeros"))
+
+cont=0
+
+for i in range(num1 +1, num2):
+    cont = cont+ i
+    
+suma= str(cont)   
+print(" La suma de dos numeros enteros ingresados es ",suma)
+
+    
+    
