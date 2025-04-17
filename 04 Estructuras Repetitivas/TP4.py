fb84b5a58d6fb329b1b6aad8a509953f8d519410
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2025-04-17 18:55:09 -0300

    TP4

---
+++ 04 Estructuras Repetitivas/TP4.py
@@ -0,0 +1,6 @@
+# 1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100 
+# (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+i=0
+for i in range(0, 101):
+    print(i)
+
