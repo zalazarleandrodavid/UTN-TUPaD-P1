3038d6dbdb938f81dd09eaf0749cbfbe0c10fd21
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2025-05-04 15:58:28 -0300

    Tp_lista - Ejercicio 10

--- 05 Funciones/Listas/Tp_Listas.py
+++ 05 Funciones/Listas/Tp_Listas.py
@@ -54,3 +54,61 @@ print(animales)
 my_lista = list(range(10,31,5))
 print(my_lista)
 
+
+# 7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores 
+# cualesquiera. 
+# autos = ["sedan", "polo", "suran", "gol"]
+
+autos = ["sedan", "polo", "suran", "gol"]
+
+autos[1]= "amarok"
+autos[2]= "golf"
+
+print(autos)
+
+
+# 8) Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append 
+# directamente. Imprimir la lista resultante por pantalla.
+
+lista_vacia = []
+
+numeros=[5,10,15]
+
+lista_vacia.append(numeros[0]*2)
+lista_vacia.append(numeros[1]*2)
+lista_vacia.append(numeros[2]*2)
+
+
+print(lista_vacia)
+
+# 9) Dada la lista “compras”, cuyos elementos representan los productos comprados por 
+# diferentes clientes: 
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], 
+# ["agua"]] 
+# a) Agregar "jugo" a la lista del tercer cliente usando append. 
+# b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente. 
+# c) Eliminar "pan" de la lista del primer cliente.  
+# d) Imprimir la lista resultante por pantalla
+
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+
+tercer_Cliente = compras[2].append("jugo")
+
+compras[1][1]= "tallarines"
+
+compras[0].remove("pan")
+
+print(compras)
+
+
+# 10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos: 
+# ● Posición lista_anidada[0]: 15 
+# ● Posición lista_anidada[1]: True 
+# ● Posición lista_anidada[2][0]: 25.5 
+# ● Posición lista_anidada[2][1]: 57.9 
+# ● Posición lista_anidada[2][2]: 30.6 
+# ● Posición lista_anidada[3]: False 
+# Imprimir la lista resultante por pantalla.
+
+lista_anidada=[[15], [True],[25.5, 57.9, 30.6,], [False]]
+print(lista_anidada)
